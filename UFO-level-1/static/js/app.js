ca6a62cef29e38d7c6834f3d1c146a1a3bc4e72e
AmyWagarCinch <gretylschmetyl@gmail.com>
2021-02-22 06:26:07 -0500

    js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Step 5: Use d3 to update each cell's text with
+// // weather report values (weekday, date, high, low)
+data.forEach(function(ufoReport) {
+    //console.log(weatherReport);
+   var row = tbody.append("tr");
+   Object.entries(ufoReport).forEach(function([key, value]) {
+//     console.log(key, value);
+      // Append a cell to the row for each value
+     // in the weather report object
+     var cell = row.append("td");
+          cell.text(value);
+   });
+ });
