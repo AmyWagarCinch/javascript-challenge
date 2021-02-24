01006c9c82d9bcade5710f0964680a5f0d15d08a
AmyWagarCinch <gretylschmetyl@gmail.com>
2021-02-23 21:37:18 -0500

    comment out errors

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,46 +19,46 @@ data.forEach(function(ufoReport) {
  });
 }
 // This function is triggered when the button is clicked
-function handleClick() {
+///function handleClick() {
     //console.log("A button was clicked!");
   
     // We can use d3 to see the object that dispatched the event
-    console.log(d3.event.target);
+    ///console.log(d3.event.target);
 
     //prevent the page from refreshing
-    d3.event.preventDefault();
+    ///d3.event.preventDefault();
 
     //select the input element and get the raw HTML node
-    var selectedDate = d3.select("#date").node().value;
-    console.log(selectedDate);
+    ///var selectedDate = d3.select("#date").node().value;
+    ///console.log(selectedDate);
 
-    let selectedData = tableData.filter(function(selectedDate){
-        return selectedDate.datetime === selectedDate;
+    ///let selectedData = tableData.filter(function(selectedDate){
+        ///return selectedDate.datetime === selectedDate;
 
-    });
-    console.log(selectedData);
+    ///});
+    ///console.log(selectedData);
 
-    var tableData = data;
-    var tbody = d3.select("tbody");
+    ///var tableData = data;
+    ///var tbody = d3.select("tbody");
 
-    function createTable(data){
-        tbody.html("");
+    ///function createTable(data){
+        ///tbody.html("");
     // Step 5: Use d3 to update each cell's text with
     // weather report values (weekday, date, high, low)
-        data.forEach(function(ufoReport) {
+        ///data.forEach(function(ufoReport) {
     
-            var row = tbody.append("tr");
-            Object.entries(ufoReport).forEach(function([key, value]) {
+            ///var row = tbody.append("tr");
+            ///Object.entries(ufoReport).forEach(function([key, value]) {
 //     console.log(key, value);
       // Append a cell to the row for each value
      // in the weather report object
-                var cell = row.append("td");
-                cell.text(value);
-   });
- });
-}
+                ///var cell = row.append("td");
+                ///cell.text(value);
+   ///});
+ ///});
+///}
 
-  }
+  ///}
   
 // We can use the `on` function in d3 to attach an event to the handler function
 button.on("click", handleClick);
